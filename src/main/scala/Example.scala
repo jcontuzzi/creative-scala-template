ccd9393823b75770a8dd29f332f25ac08a519489
am_dev <>
2017-03-30 15:15:38 -0400

    Saving

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -1,3 +1,4 @@
+import doodle.core.Color
 import doodle.core.Image._
 
 // To use this example, open the SBT console and type:
@@ -8,5 +9,7 @@ object Example {
 }
 
 object Chapter3Exercises {
+  val exercise1 = circle(100).beside(circle(100)).beside(circle(100)).on(circle(300))
+  val exercise2 = circle(10).fillColor(Color.black).on(circle(20).fillColor(Color.green)).on(circle(30)).on(circle(40).fillColor(Color.red))
 
 }

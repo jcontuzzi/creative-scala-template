e1c6fbbb056d10c852778500f0fee5dd08eef39e
am_dev <>
2017-03-30 15:42:03 -0400

    Finishing up chapter 3 exercises

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -10,10 +10,13 @@ object Example {
 
 object Chapter2Exercises {
 
+  val test = triangle(40,60).fillColor(Color.yellow.spin(doodle.core.Angle.degrees(30)))
+
 }
 
 object Chapter3Exercises {
   val exercise1 = circle(100).beside(circle(100)).beside(circle(100)).on(circle(300))
   val exercise2 = circle(10).fillColor(Color.black).on(circle(20).fillColor(Color.green)).on(circle(30).fillColor(Color.white)).on(circle(50).fillColor(Color.orange))
-  val exercise3 =
+  val exercise3 = triangle(40,60).fillColor(Color.yellow).beside(triangle(40,60).fillColor(Color.yellow.spin(doodle.core.Angle.degrees(15)))).below(triangle(40,60).fillColor(Color.yellow.spin(doodle.core.Angle.degrees(60))))
+  val exercise4 = circle(10).fillColor(Color.red).on(circle(20)).on(circle(30).fillColor(Color.red)).above(rectangle(10,30).lineWidth(3).above(rectangle(30,10).fillColor(Color.brown).above(rectangle(150,40).fillColor(Color.green))))
 }
